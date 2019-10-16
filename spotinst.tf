f225c4bcf337ae36885abaa863c39ee91f086c9c
Eric Ho <dho.eric@gmail.com>
2019-10-16 16:28:33 +0800

    Skip desired cap

--- spotinst.tf
+++ spotinst.tf
@@ -5,26 +5,23 @@ resource "spotinst_ocean_ecs" "spotinst_auto_scaling" {
   name         = "${var.project_name}-${var.env}"
   cluster_name = "${var.project_name}-${var.env}"
 
-  min_size         = "${var.spotinst_min_size}"
-  max_size         = "${var.spotinst_max_size}"
-  desired_capacity = "${var.spotinst_desired_capacity}"
+  min_size = "${var.spotinst_min_size}"
+  max_size = "${var.spotinst_max_size}"
 
-  subnet_ids = "${split(",", var.private_subnet_ids)}"
-  whitelist  = "${split(",", var.spotinst_whitelist)}"
+  # desired_capacity = "${var.spotinst_desired_capacity}"
 
-  image_id             = "${data.aws_ssm_parameter.ecs.value}"
-  security_group_ids   = ["${aws_security_group.app_sg.id}"]
-  key_pair             = "${var.deploy_key_name}"
-  user_data            = "${data.template_file.cloud_config.rendered}"
-  iam_instance_profile = "${var.iam_instance_profile}"
-
+  subnet_ids                  = "${split(",", var.private_subnet_ids)}"
+  whitelist                   = "${split(",", var.spotinst_whitelist)}"
+  image_id                    = "${data.aws_ssm_parameter.ecs.value}"
+  security_group_ids          = ["${aws_security_group.app_sg.id}"]
+  key_pair                    = "${var.deploy_key_name}"
+  user_data                   = "${data.template_file.cloud_config.rendered}"
+  iam_instance_profile        = "${var.iam_instance_profile}"
   associate_public_ip_address = false
-
   autoscaler {
     is_auto_config = true
     is_enabled     = true
   }
-
   update_policy {
     should_roll = true
 
@@ -32,7 +29,6 @@ resource "spotinst_ocean_ecs" "spotinst_auto_scaling" {
       batch_size_percentage = 100
     }
   }
-
   tags = [
     {
       key   = "Name"
