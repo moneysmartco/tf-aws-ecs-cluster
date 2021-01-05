f94775af029e817d5fc62ae86eee212ee60c6caf
Prateek Baheti <prateek.baheti@moneysmart.com>
2021-01-05 11:05:43 +0800

    Revert "Upgrade to tf 13"
    This reverts commit 73e0a54eb52000cc6c8ed09ecfd81cbb1bcfef39.

--- versions.tf
+++ versions.tf
@@ -1,18 +1,4 @@
 
 terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
-    spotinst = {
-      source = "spotinst/spotinst"
-    }
-    template = {
-      source = "hashicorp/template"
-    }
-  }
+  required_version = ">= 0.12"
 }
